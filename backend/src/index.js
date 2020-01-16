47681a09e48151b19fdd5d11183d14a9e4fe331e
Tiago D Teixeira <tiagoadmstz@yahoo.com.br>
2020-01-16 00:40:41 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:mYYDA3gMfgbGnW6H@omnistack10-ncznr.mongodb.net/test?retryWrites=true&w=majority',
 {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
     
+app.use(cors());
 app.use(express.json());
 app.use(routes);
     
